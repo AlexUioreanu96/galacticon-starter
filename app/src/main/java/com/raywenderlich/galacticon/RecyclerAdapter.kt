532146178804b08eacd03dd7009cb011f008a0d9
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-30 11:46:16 +0300

    Infating the view with onCreateViewHolder method, and adding and intent into the onClick method to make the view clickable

--- app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
+++ app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
@@ -1,6 +1,8 @@
 package com.raywenderlich.galacticon
 
+import android.content.Intent
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +22,10 @@ class RecyclerAdapter(private val photos: ArrayList<Photo>) :
 
 
         override fun onClick(v: View) {
+            val context = itemView.context
+            val showPhotoIntent = Intent(context, PhotoActivity::class.java)
+            showPhotoIntent.putExtra(PHOTO_KEY, photo)
+            context.startActivity(showPhotoIntent)
             Log.d("RecyclerView", "CLICK!")
         }
 
@@ -29,8 +35,10 @@ class RecyclerAdapter(private val photos: ArrayList<Photo>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
-//        val inflatedView = parent.inflate(R.layout.recyclerview_item_row, false)
-//        return PhotoHolder(inflatedView)
+        return PhotoHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item_row, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
